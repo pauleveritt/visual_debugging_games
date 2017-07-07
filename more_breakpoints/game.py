e69757399bad6ccdf3388e9b1ad58202a8e6a85c
Paul Everitt <pauleveritt@me.com>
2017-07-07 16:58:19 -0400

    more_breakpoints

--- more_breakpoints/game.py
+++ more_breakpoints/game.py
@@ -5,13 +5,11 @@ class MyGame(arcade.Window):
     def __init__(self, width, height, title):
         super().__init__(width, height, title)
         arcade.set_background_color(arcade.color.WHEAT)
-        self.width = width
-        self.height = height
         self.title = title
 
     def on_draw(self):
         arcade.start_render()
-        arcade.draw_text('Hello World', 100, 400, arcade.color.BLACK)
+        arcade.draw_text(self.title, 100, 400, arcade.color.BLACK)
 
 
 def main():
